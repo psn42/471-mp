db07c1415e9e6210f1cc28d6f0489710c75ab3ff
YouJun <1802kel@gmail.com>
2026-03-30 09:44:43 -0700

    Fixed bug on pack_relay_cell() and unpack_relay_cell() : Incorrect expected_relay_cell construction

--- SimpleTor_cell_test.py
+++ SimpleTor_cell_test.py
@@ -38,12 +38,34 @@ class SimpleTorStructTest(unittest.TestCase):
         relayCmd = stc.RelayCmd.DATA
         streamID = 2
         data = b'Test payload for Relay Cell packing'
-
+        data_len = len(data)
         packed_relay_cell = stc.pack_relayCell(relayCmd, streamID,data)
-        print(packed_relay_cell)
         self.assertEqual(len(packed_relay_cell), 509, "Relay Cell length is not equal to 509")
         
-        expected_relay_cell = b'\x02\x00\x00\x00\x02\x00\x00\x00\x00\x23' + data + b'\x00' * (stc.RELAY_CELL_LEN - 11-35)
+        expected_relay_cell = b'\x02\x00\x00\x00\x02\x00\x00\x00\x00\x00\x23' + data + (b'\x00' * (stc.RELAY_CELL_DATA_LEN - data_len))
+
         self.assertEqual(expected_relay_cell, packed_relay_cell, "Value of relay cell is unexpected")
+        
+    def test_unpack_relay_cell(self):
+        relayCmd = stc.RelayCmd.DATA
+        streamID = 2
+        data = b'Test payload for Relay Cell unpacking' #37 length
+        data_len = len(data)
+        packed_relay_cell = stc.pack_relayCell(relayCmd, streamID,data)
+        new_relayCmd, new_recognized, new_streamID, new_digest, new_data_len, new_data = stc.unpack_relay_cell(packed_relay_cell)
+        
+        self.assertEqual(new_relayCmd,relayCmd, "Relay Command is not matched")
+        self.assertEqual(new_recognized, 0, "Recognized is not matched")
+        self.assertEqual(new_streamID, streamID,"Stream ID is not matched")
+        self.assertEqual(new_digest, b'\x00\x00\x00\x00', "Digest is not matched")
+        self.assertEqual(new_data_len, data_len, "Data Length is not matched")
+        
+        expected_data = data + (b'\x00' * (stc.RELAY_CELL_DATA_LEN - data_len))
+
+        self.assertEqual(new_data,expected_data, "Data is not matched")
+        
+        
+        
+        
 if __name__ == '__main__':
     unittest.main()
