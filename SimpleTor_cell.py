6f51b5ed74edcb763734ed8a8aa2ccb9b7b776f6
yla766 <yla766@sfu.ca>
2026-03-29 20:42:01 -0700

    Implemented pack_relayCell()

--- SimpleTor_cell.py
+++ SimpleTor_cell.py
@@ -1,10 +1,23 @@
 import struct
 from enum import IntEnum
+import hashlib
+#length of parameters for Circuit cell
 
 CELL_LEN = 512
 CIRCID_ID_LEN = 2
 CELL_CMD_LEN = 1
 CELL_BODY_LEN = 509
+
+#length of parameters for Relay cell
+
+RELAY_CELL_LEN = 509 
+RELAY_CELL_CMD_LEN = 1 
+RELAY_CELL_RECOGNIZED_LEN = 2
+RELAY_CELL_STREAM_ID_LEN = 2
+RELAY_CELL_DIGEST_LEN = 4
+RELAY_CELL_LENGTH_LEN = 2
+RELAY_CELL_DATA_LEN = 498
+
 
 class CellCmd(IntEnum):
     PADDING = 0
@@ -23,23 +36,56 @@ class RelayCmd(IntEnum):
     EXTENDED = 7
 
 def pack_cell(circID : int, cellCmd : int, payload: bytes) -> bytes:
-    assert circID.bit_length != 16, "Unexpected Length of Circuit ID."
-    assert cellCmd.bit_length != 8, "Unexpected Length of Circuit ID."
-    assert len(payload) <= CELL_BODY_LEN, "Length of payload exceeded"
-    
-    padding = b'\x00' * (CELL_BODY_LEN - len(payload))
-    
-    
-    padded_payload = payload + padding
-    assert padded_payload
-    packed_cell = struct.pack('>HB509s', circID, cellCmd, padded_payload)
+    assert circID.bit_length <=  CIRCID_ID_LEN * 8, "pack_cell(): Unexpected Length of Circuit ID."
+    assert cellCmd.bit_length <= CELL_CMD_LEN * 8, "pack_cell(): Unexpected Length of Circuit Command."
+    assert len(payload) <= CELL_BODY_LEN, "pack_cell(): Length of payload exceeded"
+    try:
+        padding = b'\x00' * (CELL_BODY_LEN - len(payload))
+        padded_payload = payload + padding
+        packed_cell = struct.pack('>HB509s', circID, cellCmd, padded_payload)
+    except Exception as e:
+        print(f"Unexpected Error when packing Circuit Cell: {e}")
+
+    #assert len(packed_cell) == CELL_LEN, "pack_cell(): Unexpected length of Circuit Cell." 
+
     return packed_cell
 
 def unpack_cell(in_cell : bytes):
-    assert len(in_cell) == CELL_LEN, "Unexpected Length of Cell"
+    assert len(in_cell) == CELL_LEN, "unpack_cell(): Unexpected Length of Cell"
+    try:
+        circID, cellCmd, payload = struct.unpack('>HB509s', in_cell)
+    except Exception as e:
+        print(f"Unexpected Error when unpacking Circuit Cell: {e}")
 
-    circID, cellCmd, payload = struct.unpack('>HB509s', in_cell)
     return circID, cellCmd, payload
+
+
+def pack_relayCell(relayCmd :int, streamID : int, data : bytes) -> bytes:
+    data_len = len(data)
+    assert relayCmd.bit_length <= RELAY_CELL_CMD_LEN * 8, "pack_relayCell(): Unexpected Length of Relay ID."
+    assert streamID.bit_length <= RELAY_CELL_STREAM_ID_LEN * 8, "pack_relayCell(): Unexpected Length of stream ID."
+    assert data_len == RELAY_CELL_DATA_LEN, "pack_relayCell(): Unexpected Length of Data."
+
+    try:
+        padding = b'\x00' * (RELAY_CELL_DATA_LEN - data_len)
+        recognized = 0
+        digest = '\x00' * 4 # Simplified for the prototype
+        padded_data = data + padding
+        packed_relay_cell = struct.pack('>BHH4sH498s', relayCmd, recognized, streamID, digest, data_len,data,padded_data)
+    except Exception as e:
+        print(f"Unexpected Error when packing Relay Cell: {e}")   
+
+
+    
+    return packed_relay_cell
+        
+
+        
+        
+        
+    
+
+
 
 
 
