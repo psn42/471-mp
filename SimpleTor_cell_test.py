30283e46592e20647bd8f0bad3a948ad31097bde
YouJun <1802kel@gmail.com>
2026-03-30 09:00:26 -0700

    Created test for pack_relay_cell()

--- SimpleTor_cell_test.py
+++ SimpleTor_cell_test.py
@@ -20,7 +20,7 @@ class SimpleTorStructTest(unittest.TestCase):
     def test_unpack_cell(self):
         circID = 1
         cmd = stc.CellCmd.RELAY
-        payload = b'Test payload for padding'
+        payload = b'Test payload for Circuit Cell packing'
 
         padding = b'\x00' * (stc.CELL_BODY_LEN - len(payload))
         padded_payload = payload + padding
@@ -34,7 +34,16 @@ class SimpleTorStructTest(unittest.TestCase):
         self.assertEqual(new_payload,padded_payload, "Payload is not matched")
         
         
+    def test_pack_relay_cell(self):
+        relayCmd = stc.RelayCmd.DATA
+        streamID = 2
+        data = b'Test payload for Relay Cell packing'
 
-
+        packed_relay_cell = stc.pack_relayCell(relayCmd, streamID,data)
+        print(packed_relay_cell)
+        self.assertEqual(len(packed_relay_cell), 509, "Relay Cell length is not equal to 509")
+        
+        expected_relay_cell = b'\x02\x00\x00\x00\x02\x00\x00\x00\x00\x23' + data + b'\x00' * (stc.RELAY_CELL_LEN - 11-35)
+        self.assertEqual(expected_relay_cell, packed_relay_cell, "Value of relay cell is unexpected")
 if __name__ == '__main__':
     unittest.main()
